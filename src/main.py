1bc86d07d9aedc96a96ca62379f9c69d6b1bb7ce
seongpil0948 <seongpil0948@gmail.com>
2021-02-04 17:01:36 +0900

    python version fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 from crawler import Crawler
+from sys import version_info as v
+from sys import exit
+
+if v.major != 3 or v.minor != 9 or v.micro != 1:
+    print("Python 3.9.1 is required.")
+    exit(1)
+else:
+    print('success')
 
 c = Crawler(code=66080)
 c.go()
